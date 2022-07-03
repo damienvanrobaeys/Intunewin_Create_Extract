28a6c8380dbd006460693d1f747767019c42e715
Damien Van Robaeys <damien.vanrobaeys@gmail.com>
2022-07-03 22:10:21 +0200

    Add files via upload

--- Install on desktop (in case of issue with EXE)/Install_tool.ps1
+++ Install on desktop (in case of issue with EXE)/Install_tool.ps1
@@ -5,14 +5,31 @@
 #***************************************************************************************************************
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | out-null
 
+$IntuneWinAppUtil_Link =  "https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/raw/master/IntuneWinAppUtil.exe"
+Try
+	{
+		$IntuneWinAppUtil_File = "$env:temp\IntuneWinAppUtil.exe"
+		Invoke-WebRequest -Uri $IntuneWinAppUtil_Link -OutFile $IntuneWinAppUtil_File -UseBasicParsing | out-null
+		$Download_Status = $True
+	}
+Catch
+	{
+		$Download_Status = $False								
+	}
+
+
 $Current_Folder = split-path $MyInvocation.MyCommand.Path
 $Sources = $Current_Folder + "\" + "Sources\*"
 If(test-path $Sources)
 	{	
 		$ProgData = $env:ProgramData
-		$Destination_folder = "$ProgData\Intunewin_Build_Extract"
-		If(!(test-path $Destination_folder)){new-item $Destination_folder -type directory -force | out-null}
+		# $Destination_folder = "$ProgData\Intunewin_Build_Extract"
+		$Destination_folder = "$env:LOCALAPPDATA\Intunewin_Build_Extract"
+
+		If(test-path $Destination_folder){remove-item $Destination_folder -recurse -force | out-null}
+		new-item $Destination_folder -type directory -force | out-null
 		copy-item $Sources $Destination_folder -force -recurse
+		copy-item $IntuneWinAppUtil_File $Destination_folder -force -recurse
 		Get-Childitem -Recurse $Destination_folder | Unblock-file	
 		
 		$Get_Current_user = (gwmi win32_computersystem).username
@@ -21,11 +38,20 @@ If(test-path $Sources)
 
 		$Get_Tool_Shortcut = "$Destination_folder\Intunewin Build and Extract.lnk"		
 		$Get_Desktop_Profile = [environment]::GetFolderPath('Desktop')
+		$Desktop_LNK = "$Get_Desktop_Profile\Intunewin Build and Extract.lnk"
 		copy-item $Get_Tool_Shortcut $Get_Desktop_Profile -Force
-
-		[System.Windows.Forms.MessageBox]::Show("Intunewin Build and Extract has been installed.`nA shortcut has been added on your Desktop.")			
+		
+		$Shell = New-Object -ComObject ("WScript.Shell")
+		$Shortcut = $Shell.CreateShortcut($Desktop_LNK)
+		$shortcut.IconLocation = "$Destination_folder\logo.ico"
+		$Shortcut.Save()
+		
+		[System.Windows.Forms.MessageBox]::Show("Intunewin Build and Extract has been installed`n`nA shortcut has been added on your Desktop")			
 	}
 Else
 	{
 		[System.Windows.Forms.MessageBox]::Show("It seems you don't have dowloaded all the folder structure.`nThe folder Sources is missing !!!")	
 	}
+
+
+
--- Install on desktop (in case of issue with EXE)/Sources/IntuneWin_Generator.ps1
+++ Install on desktop (in case of issue with EXE)/Sources/IntuneWin_Generator.ps1
@@ -8,26 +8,14 @@
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
-# $Run_As_Admin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-# If($Run_As_Admin -eq $False)
-	# {
-		# [System.Windows.Forms.MessageBox]::Show("Please run the tool with admin rights")
-		# break		
-	# }
-
-
 $Global:Current_Folder = split-path $MyInvocation.MyCommand.Path
 
 [System.Reflection.Assembly]::LoadWithPartialName('presentationframework') | out-null
 [System.Reflection.Assembly]::LoadFrom("$Current_Folder\MahApps.Metro.dll") | out-null  
 [System.Reflection.Assembly]::LoadFrom("$Current_Folder\MahApps.Metro.IconPacks.dll") | out-null  
-
-#########################################################################
-#                        Load Main Panel                                #
-#########################################################################
+[System.Reflection.Assembly]::LoadFrom("$Current_Folder\LoadingIndicators.WPF.dll") | out-null
 
 $Global:pathPanel= split-path -parent $MyInvocation.MyCommand.Definition
-$Global:Current_Folder = split-path $MyInvocation.MyCommand.Path
 
 function LoadXaml ($filename){
     $XamlLoader=(New-Object System.Xml.XmlDocument)
@@ -35,28 +23,100 @@ function LoadXaml ($filename){
     return $XamlLoader
 }
 
-
 $XamlMainWindow=LoadXaml($pathPanel+"\IntuneWin_Generator.xaml")
 $reader = (New-Object System.Xml.XmlNodeReader $XamlMainWindow)
 $Form = [Windows.Markup.XamlReader]::Load($reader)
 
-
-$browse_package_folder = $Form.FindName("browse_package_folder") 
-$Change_Theme = $Form.FindName("Change_Theme") 
-$browse_package_folder_textbox = $Form.FindName("browse_package_folder_textbox") 
-$file_to_run = $Form.FindName("file_to_run") 
-$browse_output = $Form.FindName("browse_output") 
-$browse_output_txtbox = $Form.FindName("browse_output_txtbox") 
-$build = $Form.FindName("build") 
-$package_name = $Form.FindName("package_name") 
+$XamlMainWindow.SelectNodes("//*[@Name]") | %{
+    try {Set-Variable -Name "$($_.Name)" -Value $Form.FindName($_.Name) -ErrorAction Stop}
+    catch{throw}
+}
 
 
+#*******************************************************************************************************************************************************************************************************
+#																						 BARRE DE PROGRESSION
+#*******************************************************************************************************************************************************************************************************
 
-$browse_intunewin = $Form.FindName("browse_intunewin") 
-$intunewin_sources_textbox = $Form.FindName("intunewin_sources_textbox") 
-$browse_output_intunewin = $Form.FindName("browse_output_intunewin") 
-$browse_output_txtbox_inunewin = $Form.FindName("browse_output_txtbox_inunewin") 
-$extract = $Form.FindName("extract") 
+$syncProgress = [hashtable]::Synchronized(@{})
+$childRunspace = [runspacefactory]::CreateRunspace()
+$childRunspace.ApartmentState = "STA"
+$childRunspace.ThreadOptions = "ReuseThread"         
+$childRunspace.Open()
+$childRunspace.SessionStateProxy.SetVariable("syncProgress",$syncProgress)          
+$PsChildCmd = [PowerShell]::Create().AddScript({   
+    [xml]$xaml = @"
+	<Controls:MetroWindow 
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+		xmlns:i="http://schemas.microsoft.com/expression/2010/interactivity"				
+		xmlns:Controls="clr-namespace:MahApps.Metro.Controls;assembly=MahApps.Metro"	
+		xmlns:loadin="clr-namespace:LoadingIndicators.WPF;assembly=LoadingIndicators.WPF"				
+        Name="WindowProgress" 
+		WindowStyle="None" AllowsTransparency="True" UseNoneWindowStyle="True"	
+		Width="600" Height="300" 
+		WindowStartupLocation ="CenterScreen" Topmost="true"
+		BorderBrush="Gray" ResizeMode="NoResize"
+		>
+
+<Window.Resources>
+	<ResourceDictionary>
+		<ResourceDictionary.MergedDictionaries>
+			<!-- LoadingIndicators resources -->
+			<ResourceDictionary Source="pack://application:,,,/LoadingIndicators.WPF;component/Styles.xaml"/>	
+			<!-- Mahapps resources -->
+			<ResourceDictionary Source="pack://application:,,,/MahApps.Metro;component/Styles/Controls.xaml" />
+			<ResourceDictionary Source="pack://application:,,,/MahApps.Metro;component/Styles/Fonts.xaml" />
+			<ResourceDictionary Source="pack://application:,,,/MahApps.Metro;component/Styles/Colors.xaml" />
+			<ResourceDictionary Source="pack://application:,,,/MahApps.Metro;component/Styles/Accents/Cobalt.xaml" />
+			<ResourceDictionary Source="pack://application:,,,/MahApps.Metro;component/Styles/Accents/BaseDark.xaml" />		
+		</ResourceDictionary.MergedDictionaries>
+	</ResourceDictionary>
+</Window.Resources>			
+
+	<Window.Background>
+		<SolidColorBrush Opacity="0.7" Color="#0077D6"/>
+	</Window.Background>	
+		
+	<Grid>	
+		<StackPanel Orientation="Vertical" VerticalAlignment="Center" HorizontalAlignment="Center">		
+			<StackPanel Orientation="Vertical" HorizontalAlignment="Center" Margin="0,0,0,0">	
+			<!--	<Controls:ProgressRing IsActive="True" Margin="0,0,0,0"  Foreground="White" Width="50"/> -->
+				<loadin:LoadingIndicator Margin="0,5,0,0" Name="ArcsRing" SpeedRatio="1" Foreground="White" IsActive="True" Style="{DynamicResource LoadingIndicatorArcsRingStyle}"/>
+			</StackPanel>								
+			
+			<StackPanel Orientation="Vertical" HorizontalAlignment="Center" Margin="0,20,0,0">				
+				<Label Name="ProgressStep" Content="Getting latest IntuneWinAppUtil.exe version" FontSize="17" Margin="0,0,0,0" Foreground="White"/>	
+				<Label HorizontalAlignment="Center" Content="Please wait ..." FontSize="17" Margin="0,0,0,0" Foreground="White"/>	
+
+			</StackPanel>			
+		</StackPanel>		
+		
+	</Grid>
+</Controls:MetroWindow>
+"@
+  
+    $reader=(New-Object System.Xml.XmlNodeReader $xaml)
+    $syncProgress.Window=[Windows.Markup.XamlReader]::Load( $reader )
+    $syncProgress.Label = $syncProgress.window.FindName("ProgressStep")	
+
+    $syncProgress.Window.ShowDialog() #| Out-Null
+    $syncProgress.Error = $Error
+})
+
+
+
+################ Launch Progress Bar  ########################  
+Function Launch_modal_progress{    
+    $PsChildCmd.Runspace = $childRunspace
+    $Script:Childproc = $PsChildCmd.BeginInvoke()
+	
+}
+
+################ Close Progress Bar  ########################  
+Function Close_modal_progress{
+    $syncProgress.Window.Dispatcher.Invoke([action]{$syncProgress.Window.close()})
+    $PsChildCmd.EndInvoke($Script:Childproc) | Out-Null
+}
 
 
 $browse_output_txtbox.IsEnabled = $False
@@ -65,7 +125,33 @@ $browse_output_txtbox_inunewin.IsEnabled = $False
 
 $object = New-Object -comObject Shell.Application  
 
-	
+Launch_modal_progress
+
+$IntuneWinAppUtil_Link =  "https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/raw/master/IntuneWinAppUtil.exe"
+
+$Last_Version_Length = (Invoke-WebRequest -method "Head" $IntuneWinAppUtil_Link -UseBasicParsing | Select Headers -ExpandProperty Headers)."Content-Length"
+$Current_Version_Length = (get-item "$Current_Folder\IntuneWinAppUtil.exe").length
+
+
+
+If($Current_Version_Length -ne $Last_Version_Length)
+	{
+		$IntuneWinAppUtil_File = "$env:temp\IntuneWinAppUtil.exe"
+		Invoke-WebRequest -Uri $IntuneWinAppUtil_Link -OutFile $IntuneWinAppUtil_File -UseBasicParsing | out-null	
+	}
+Else
+	{
+		$IntuneWinAppUtil_File = "$Current_Folder\IntuneWinAppUtil.exe"
+	}
+
+$IntuneWinAppUtilDecoder = "$Current_Folder\IntuneWinAppUtilDecoder.exe"
+$IntuneWin_Ver = "IntuneWinAppUtil: " + [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$IntuneWinAppUtil_File").FileVersion
+$IntuneWinDecoder_Ver = "Decoder: " + [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$IntuneWinAppUtilDecoder").FileVersion
+
+$IntuneWinApp_Ver.Content = $IntuneWin_Ver + " / " + $IntuneWinDecoder_Ver
+
+Close_modal_progress
+
 #===========================================================================
 # Declare the change_theme button and action on this button
 #===========================================================================
@@ -85,8 +171,6 @@ $Change_Theme.Add_Click({
 #===========================================================================
 # Declare the change_theme button and action on this button
 #===========================================================================
-
-
 
 
 $browse_package_folder.Add_Click({		
@@ -151,6 +235,20 @@ $browse_output_intunewin.Add_Click({
 })
 
 
+$build.add_Click({
+
+    Start-Process -WindowStyle hidden -FilePath $IntuneWinAppUtil_File -ArgumentList "-c `"$($Sources_Folder)`" -s `"$($Intunewin_File_To_Run)`" -o `"$($Package_output_folder)`" -q" -Wait
+	$PackageNewName = $package_name.Text.ToString()
+	If($PackageNewName -ne "")
+		{
+			$Get_File_ShortName = (get-item $File_Path).BaseName			
+			$IntuneWin_FileToRename = "$Package_output_folder\$Get_File_ShortName.intunewin"
+			$Intunewin_NewName = "$PackageNewName.intunewin"
+			Rename-Item $IntuneWin_FileToRename $Intunewin_NewName -Force			
+		}
+	[MahApps.Metro.Controls.Dialogs.DialogManager]::ShowModalMessageExternal($Form,"Intunewin creation","Your intunewin package has been correctly created")
+})
+
 $extract.add_Click({
 	& .\IntuneWinAppUtilDecoder.exe $Intunewin_Full_path -s	
 	$Intunewin_Extract_Directory = (get-item $Intunewin_Full_path).Directory	
@@ -168,28 +266,5 @@ $extract.add_Click({
 	[MahApps.Metro.Controls.Dialogs.DialogManager]::ShowModalMessageExternal($Form,"Intunewin extraction","Your intunewin package has been correctly extracted")
 
 })
-
-
-
-
-
-$build.add_Click({
-    Start-Process -WindowStyle hidden -FilePath "$Current_Folder\IntuneWinAppUtil.exe" -ArgumentList "-c `"$($Sources_Folder)`" -s `"$($Intunewin_File_To_Run)`" -o `"$($Package_output_folder)`" -q" -Wait
-	$PackageNewName = $package_name.Text.ToString()
-	If($PackageNewName -ne "")
-		{
-			$Get_File_ShortName = (get-item $File_Path).BaseName			
-			$IntuneWin_FileToRename = "$Package_output_folder\$Get_File_ShortName.intunewin"
-			$Intunewin_NewName = "$PackageNewName.intunewin"
-			Rename-Item $IntuneWin_FileToRename $Intunewin_NewName -Force			
-		}
-	[MahApps.Metro.Controls.Dialogs.DialogManager]::ShowModalMessageExternal($Form,"Intunewin creation","Your intunewin package has been correctly created")
-})
-
-
-
-
-
-
 
 $Form.ShowDialog() | Out-Null
