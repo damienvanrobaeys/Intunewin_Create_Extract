34120798d8d2dd73875bf5cbf84be15d6178a3db
Damien Van Robaeys <damien.vanrobaeys@gmail.com>
2023-02-27 13:41:56 +0100

    Add files via upload

--- Install on desktop (in case of issue with EXE)/Install_tool.ps1
+++ Install on desktop (in case of issue with EXE)/Install_tool.ps1
@@ -5,47 +5,41 @@
 #***************************************************************************************************************
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | out-null
 
-$IntuneWinAppUtil_Link =  "https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/raw/master/IntuneWinAppUtil.exe"
-Try
-	{
-		$IntuneWinAppUtil_File = "$env:temp\IntuneWinAppUtil.exe"
-		Invoke-WebRequest -Uri $IntuneWinAppUtil_Link -OutFile $IntuneWinAppUtil_File -UseBasicParsing | out-null
-		$Download_Status = $True
-	}
-Catch
-	{
-		$Download_Status = $False								
-	}
-
-
 $Current_Folder = split-path $MyInvocation.MyCommand.Path
 $Sources = $Current_Folder + "\" + "Sources\*"
 If(test-path $Sources)
 	{	
-		$ProgData = $env:ProgramData
-		# $Destination_folder = "$ProgData\Intunewin_Build_Extract"
 		$Destination_folder = "$env:LOCALAPPDATA\Intunewin_Build_Extract"
 
 		If(test-path $Destination_folder){remove-item $Destination_folder -recurse -force | out-null}
 		new-item $Destination_folder -type directory -force | out-null
 		copy-item $Sources $Destination_folder -force -recurse
-		copy-item $IntuneWinAppUtil_File $Destination_folder -force -recurse
+		
+		$IntuneWinAppUtil_Link =  "https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/raw/master/IntuneWinAppUtil.exe"
+		$IntuneWinAppUtil_File = "$Destination_folder\IntuneWinAppUtil.exe"
+		Invoke-WebRequest -Uri $IntuneWinAppUtil_Link -OutFile $IntuneWinAppUtil_File -UseBasicParsing | out-null
+
+		$Decode_Link = "https://github.com/okieselbach/Intune/raw/master/IntuneWinAppUtilDecoder/IntuneWinAppUtilDecoder/bin/Release/IntuneWinAppUtilDecoder.exe"
+		$DecodeApp_File = "$Destination_folder\IntuneWinAppUtilDecoder.exe"
+		Invoke-WebRequest -Uri $Decode_Link -OutFile $DecodeApp_File -UseBasicParsing | out-null
+		
 		Get-Childitem -Recurse $Destination_folder | Unblock-file	
 		
-		$Get_Current_user = (gwmi win32_computersystem).username
-		$Get_Current_user_Name = $Get_Current_user.Split("\")[1]		
-		$User_Desktop_Profile = "C:\Users\$Get_Current_user_Name\Desktop"
-
-		$Get_Tool_Shortcut = "$Destination_folder\Intunewin Build and Extract.lnk"		
-		$Get_Desktop_Profile = [environment]::GetFolderPath('Desktop')
-		$Desktop_LNK = "$Get_Desktop_Profile\Intunewin Build and Extract.lnk"
-		copy-item $Get_Tool_Shortcut $Get_Desktop_Profile -Force
-		
-		$Shell = New-Object -ComObject ("WScript.Shell")
-		$Shortcut = $Shell.CreateShortcut($Desktop_LNK)
+		# Creatre the LNK file
+		$LNK_File = "$Destination_folder\Intunewin Build and Extract.lnk"
+		$WshShell = New-Object -comObject WScript.Shell
+		$Shortcut = $WshShell.CreateShortcut("$LNK_File")
+		$Target_Path = "C:\Windows\System32\cmd.exe"
+		$Target_Arguments = "/c start /min powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File %LOCALAPPDATA%\Intunewin_Build_Extract\IntuneWin_Generator.ps1"
+		$Shortcut.TargetPath = $Target_Path
+		$Shortcut.Arguments = $Target_Arguments
 		$shortcut.IconLocation = "$Destination_folder\logo.ico"
-		$Shortcut.Save()
-		
+		$shortcut.WorkingDirectory = $Destination_folder		
+		$Shortcut.Save()		
+
+		$Get_Desktop_Profile = [environment]::GetFolderPath('Desktop')
+		copy-item $LNK_File $Get_Desktop_Profile -Force
+
 		[System.Windows.Forms.MessageBox]::Show("Intunewin Build and Extract has been installed`n`nA shortcut has been added on your Desktop")			
 	}
 Else
